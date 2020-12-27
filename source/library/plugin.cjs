d9a706687b72dbd9444c6e3c08752bbd4e45ad2e
virtualpatterns <code.virtualpatterns@gmail.com>
2020-12-26 21:45:08 -0500

    upgrades, enter/exit, nec. eslintrc

--- source/library/plugin.cjs
+++ source/library/plugin.cjs
@@ -14,6 +14,10 @@ class Plugin {
 
       let pluginObject = {}
 
+      pluginObject.manipulateOptions = function(option, parserOption) {
+        console.dir(parserOption)
+      }
+
       pluginObject.pre = function(state) { visitorInstance.onBeforeNode(state) }
       pluginObject.post = function(state) { visitorInstance.onAfterNode(state) }
 
@@ -21,7 +25,37 @@ class Plugin {
       let visitorObject = {}
 
       nodeType.forEach((nodeType) => {
-        visitorObject[nodeType] = function(path, state) { visitorInstance[`on${PascalCase(nodeType)}Node`](path, state, this) }
+
+        let onNodeName = `on${PascalCase(nodeType)}Node`
+
+        if (onNodeName in visitorInstance) {
+          visitorObject[nodeType] = function(path, state) { visitorInstance[onNodeName](path, state, this) }
+        }
+
+        let onEnterNodeName = `onEnter${PascalCase(nodeType)}Node`
+        let onExitNodeName = `onExit${PascalCase(nodeType)}Node`
+
+        if (onEnterNodeName in visitorInstance ||
+            onExitNodeName in visitorInstance) {
+
+          visitorObject[nodeType] = { 
+
+            enter(path, state) {
+              if (onEnterNodeName in visitorInstance) {
+                visitorInstance[onEnterNodeName](path, state, this)
+              }
+            }, 
+
+            exit(path, state) {
+              if (onExitNodeName in visitorInstance) {
+                visitorInstance[onExitNodeName](path, state, this)
+              }
+            } 
+
+          }
+
+        }
+
       })
 
       pluginObject.visitor = visitorObject
--- source/test/library/visitor.test.js
+++ source/test/library/visitor.test.js
@@ -4,12 +4,46 @@ import Test from 'ava'
 
 const Require = CreateRequire(import.meta.url)
 
-Test('plugins: [ reverse-identifier ]', async (test) => {
+Test.only('plugins: [ reverse-identifier ]', async (test) => {
 
   let codeIn = 'console.log(\'Hello, world!\')'
   let option = { 
     'plugins': [ 
       Require.resolve('./reverse-identifier/index.cjs')
+    ]
+  }
+
+  let { code: actualCodeOut } = await Babel.transformAsync(codeIn, option)
+  let expectedCodeOut = 'elosnoc.gol(\'Hello, world!\');'
+
+  // test.log(actualCodeOut)
+  test.is(actualCodeOut, expectedCodeOut)
+
+})
+
+Test('plugins: [ reverse-identifier-on-enter ]', async (test) => {
+
+  let codeIn = 'console.log(\'Hello, world!\')'
+  let option = { 
+    'plugins': [ 
+      Require.resolve('./reverse-identifier-on-enter/index.cjs')
+    ]
+  }
+
+  let { code: actualCodeOut } = await Babel.transformAsync(codeIn, option)
+  let expectedCodeOut = 'elosnoc.gol(\'Hello, world!\');'
+
+  // test.log(actualCodeOut)
+  test.is(actualCodeOut, expectedCodeOut)
+
+})
+
+Test('plugins: [ reverse-identifier-on-exit ]', async (test) => {
+
+  let codeIn = 'console.log(\'Hello, world!\')'
+  let option = { 
+    'plugins': [ 
+      Require.resolve('./reverse-identifier-on-exit/index.cjs')
     ]
   }
 
