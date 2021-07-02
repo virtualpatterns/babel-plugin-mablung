6d8b563e6872f4b02cfb09074fa9e279c72bc463
virtualpatterns.com <code@virtualpatterns.com>
2021-07-01 23:12:50 -0400

    removing release from git

--- release/sandbox/cc.js
+++
@@ -1,7 +0,0 @@
-import DefaultChangeCase, * as ModuleChangeCase from 'change-case';
-debugger;
-const { pascalCase: PascalCase } = DefaultChangeCase || ModuleChangeCase;
-
-// use default for js
-
-//# sourceMappingURL=cc.js.map
--- release/sandbox/cd.cjs
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-console.log(require.resolve('@virtualpatterns/mablung-check-dependency/package.json'));
-
-//# sourceMappingURL=cd.cjs.map
